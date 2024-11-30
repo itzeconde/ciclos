e2e9a5c09effd0e9ce2a9cbdafb7aacc9b6aef82
itzeconde <chelconde46@gmail.com>
2024-11-29 21:30:46 -0600

    Add files via upload

---
+++ ejercicio4.py
@@ -0,0 +1,10 @@
+
+for numero in range(1, 6):  
+    print(f"Tabla de multiplicar del {numero}:")  
+    
+   
+    for i in range(1, 11):  
+        resultado = numero * i
+        print(f"{numero} x {i} = {resultado}")  
+    
+    print() #sirve para dejar espacios en blanco
